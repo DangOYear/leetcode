110c63fcb435aff296c95e2621c5d0a1b89ee5f4
ShengyunYu <yushengyun2013@163.com>
2019-12-09 10:31:49 +0800

    Date:20191209-1 Info:0814 1281
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

---
+++ src/LeetCode1281.java
@@ -0,0 +1,18 @@
+package src;
+
+public class LeetCode1281 {
+    class Solution {
+        public int subtractProductAndSum(int n) {
+            int product = 1;
+            int sum = 0;
+
+            while (n > 0) {
+                int num = n % 10;
+                n /= 10;
+                product *= num;
+                sum += num;
+            }
+            return product - sum;
+        }
+    }
+}
