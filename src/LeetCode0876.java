0c10fe4c42e66860cf3ce0dcbdf7c3812605a6b8
ShengyunYu <yushengyun2013@163.com>
2019-12-09 14:43:35 +0800

    Date:20191209-3 Info:0876 0922
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

---
+++ src/LeetCode0876.java
@@ -0,0 +1,27 @@
+package src;
+
+
+import baseclass.ListNode;
+
+public class LeetCode0876 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+    }
+}
