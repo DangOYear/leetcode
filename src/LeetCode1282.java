a588763c1e23b5e9db4f1b04b10ca37179bb785f
ShengyunYu <yushengyun2013@163.com>
2019-12-10 11:56:20 +0800

    Date:20191210-1 Info:1200
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

---
+++ src/LeetCode1282.java
@@ -0,0 +1,19 @@
+package src;
+
+import java.util.*;
+
+public class LeetCode1282 {
+    class Solution {
+        public List<List<Integer>> groupThePeople(int[] groupSizes) {
+            List<List<Integer>> res = new LinkedList<>();
+            Map<Integer, List<Integer>> map = new HashMap<>();
+
+            for (int i = 0; i < groupSizes.length; i++) {
+                if (map.containsKey(groupSizes[i])){
+
+                }
+            }
+            return res;
+        }
+    }
+}
