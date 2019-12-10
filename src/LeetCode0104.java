0b80be25906446a76d3d7c6c336dc292b6c7562d
ShengyunYu <yushengyun2013@163.com>
2019-12-10 12:27:37 +0800

    Date:20191210-2 Info:0104 0136 0872 0877 0912
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

---
+++ src/LeetCode0104.java
@@ -0,0 +1,23 @@
+package src;
+
+
+import baseclass.TreeNode;
+
+public class LeetCode0104 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if (root == null)
+                return 0;
+            return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        }
+    }
+}
---
+++ src/LeetCode0877.java
@@ -0,0 +1,9 @@
+package src;
+
+public class LeetCode0877 {
+    class Solution {
+        public boolean stoneGame(int[] piles) {
+            return true;
+        }
+    }
+}
