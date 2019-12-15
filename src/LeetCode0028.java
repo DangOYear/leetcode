a10c47080ebdf1a15d45bab7078cbc97eeb267e2
ShengyunYu <yushengyun2013@163.com>
2019-12-15 11:01:21 +0800

    Date:20191215-1 Info:0028 0559 1051 1287
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

---
+++ src/LeetCode0028.java
@@ -0,0 +1,12 @@
+package src;
+
+public class LeetCode0028 {
+
+    class Solution {
+        public int strStr(String haystack, String needle) {
+            return haystack.indexOf(needle);
+        }
+    }
+
+
+}
