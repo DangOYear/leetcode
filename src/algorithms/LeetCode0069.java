fd342501f172a782c211977826ab04bc3e88526c
yushengyun <yushengyun2013@163.com>
2020-04-02 22:53:06 +0800

    Date:20200402-1 Info: Algorithm:0069 0367 0374
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

---
+++ src/algorithms/LeetCode0069.java
@@ -0,0 +1,11 @@
+package src.algorithms;
+
+public class LeetCode0069 {
+    class Solution {
+        public int mySqrt(int x) {
+            int res = (int)Math.sqrt(x);
+            return res;
+        }
+    }
+
+}
---
+++ src/algorithms/LeetCode0367.java
@@ -0,0 +1,15 @@
+package src.algorithms;
+
+public class LeetCode0367 {
+    class Solution {
+        public boolean isPerfectSquare(int num) {
+            int i = 1;
+            while (num > 0) {
+                num -= i;
+                i += 2;
+            }
+
+            return num == 0;
+        }
+    }
+}
