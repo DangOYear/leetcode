4bd4b7c644ab99e5ce04a4027e502178a3e45801
yushengyun <yushengyun2013@163.com>
2020-05-09 19:53:35 +0800

    Date:20200509-1 Info: Algorithm:0033
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

---
+++ src/algorithms/LeetCode0033.java
@@ -0,0 +1,35 @@
+package src.algorithms;
+
+public class LeetCode0033 {
+
+    class Solution {
+        public int find(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            int mid = 0;
+            while (left <= right) {
+                mid = (left + right) / 2;
+                if (nums[mid] == target)
+                    return mid;
+                if (nums[left] <= nums[mid]) {
+                    if (nums[left] <= target && target <= nums[mid]) {
+                        right = mid - 1;
+                    }else {
+                        left = mid + 1;
+                    }
+                }else {
+                    if (nums[mid] < target && target <= nums[right]) {
+                        left = mid + 1;
+                    }else {
+                        right = mid - 1;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public int search(int[] nums, int target) {
+            return find(nums, target);
+        }
+    }
+}
--- src/algorithms/LeetCode0069.java
+++ src/algorithms/LeetCode0069.java
@@ -3,9 +3,20 @@ package src.algorithms;
 public class LeetCode0069 {
     class Solution {
         public int mySqrt(int x) {
-            int res = (int)Math.sqrt(x);
+            int l = 0;
+            int r = x;
+            int res = -1;
+
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if ((long)mid * mid <= x) {
+                    res = mid;
+                    l = mid + 1;
+                }else {
+                    r = mid - 1;
+                }
+            }
             return res;
         }
     }
-
 }
