9c3d49cda0fef7c0725fa0901e0419a10b8d3946
yushengyun <yushengyun2013@163.com>
2020-05-11 00:55:39 +0800

    Date:20200510-1 Info: Algorithm:0015
    Signed-off-by: ShengyunYu <yushengyun2013@163.com>

---
+++ src/algorithms/LeetCode0022.java
@@ -0,0 +1,16 @@
+package src.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode0022 {
+    class Solution {
+        public List<String> generateParenthesis(int n) {
+            List<String> res = new ArrayList<>();
+
+
+
+        }
+    }
+
+}
--- src/algorithms/LeetCode0172.java
+++ src/algorithms/LeetCode0172.java
@@ -3,11 +3,19 @@ package src.algorithms;
 public class LeetCode0172 {
     class Solution {
         public int trailingZeroes(int n) {
-            int count = 0;
+            int res = 0;
             for (int i = 1; i <= n; ++i) {
-
+                int N = i;
+                while (N > 0) {
+                    if (N % 5 == 0) {
+                        ++res;
+                        N /= 5;
+                    }else {
+                        break;
+                    }
+                }
             }
-            return -1;
+            return res;
         }
 
     }
