a6f1933277d482118447d4ff86e6883d8c57da28
yushengyun <yushengyun2013@163.com>
2020-06-08 10:35:34 +0800

    Date:20200608-1
    Info:
    Algorithm:1052 1410

---
+++ src/algorithms/LeetCode1410.java
@@ -0,0 +1,10 @@
+package src.algorithms;
+
+public class LeetCode1410 {
+    class Solution {
+        public String entityParser(String text) {
+            return text.replace("&quot;", "\"").replace("&apos;", "'").replace("&gt;", ">").replace("&lt;", "<").replace("&frasl;", "/").replace("&amp;", "&");
+        }
+    }
+
+}
