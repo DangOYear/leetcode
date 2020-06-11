ead1cd7b799724a1dbd7505340e5f70f431a53cf
ShengyunYu <yushengyun2013@163.com>
2020-06-11 10:16:27 +0800

    Date:20200611-1
    Info:
    Algorithm:0050 0343
    CodingInterviews:0014I 0014II 0016

---
+++ src/algorithms/LeetCode0343.java
@@ -0,0 +1,23 @@
+package src.algorithms;
+
+public class LeetCode0343 {
+    class Solution {
+        public int integerBreak(int n) {
+            if (n == 2) {
+                return 1;
+            }
+            if (n == 3) {
+                return 2;
+            }
+
+            int mod = 1000000007;
+            long res = 1;
+            while (n > 4) {
+                res *= 3;
+                res %= mod;
+                n -= 3;
+            }
+            return (int)(res * n % mod);
+        }
+    }
+}
