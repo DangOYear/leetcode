c84f8c4699ce011a4e3a48f83ccd7def04647d7e
yushengyun <yushengyun2013@163.com>
2020-08-03 01:37:28 +0800

    Date:20200803-1
    Info:
    Algorithm:0200 0448
    CodingInterviews:
    CrakingTheCodingInterview:

---
+++ src/algorithms/LeetCode0200.java
@@ -0,0 +1,42 @@
+package src.algorithms;
+
+public class LeetCode0200 {
+    class Solution {
+
+        void dfs(char[][] grid, int x, int y) {
+            int row = grid.length;
+            int col = grid[0].length;
+
+            if (x < 0 || x >= row || y < 0 || y >= col || grid[x][y] == '0') {
+                return;
+            }
+            grid[x][y] = '0';
+            int directX[] = new int[]{1, -1, 0, 0};
+            int directY[] = new int[]{0, 0, -1, 1};
+
+            for (int i = 0; i < 4; ++i) {
+                int dx = x + directX[i];
+                int dy = y + directY[i];
+                dfs(grid, dx, dy);
+            }
+        }
+
+        public int numIslands(char[][] grid) {
+            if (grid == null || grid.length == 0) {
+                return 0;
+            }
+            int numIsland = 0;
+            int row = grid.length;
+            int col = grid[0].length;
+            for (int i = 0; i < row; ++i) {
+                for (int j = 0; j < col; ++j) {
+                    if (grid[i][j] == '1') {
+                        ++numIsland;
+                        dfs(grid, i, j);
+                    }
+                }
+            }
+            return numIsland;
+        }
+    }
+}
---
+++ src/algorithms/LeetCode0347.java
@@ -0,0 +1,19 @@
+package src.algorithms;
+
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class LeetCode0347 {
+    class Solution {
+        public int[] topKFrequent(int[] nums, int k) {
+            HashMap<Integer, Integer> count = new HashMap<>();
+            for (int x : nums) {
+                count.put(x, count.getOrDefault(x, 0) + 1);
+            }
+            PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>((n1, n2) -> {count.get(n1) - count.get(n2)});
+            for (int key : count.keySet()) {
+                
+            }
+        }
+    }
+}
