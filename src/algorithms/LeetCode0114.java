25df359e04d19a5fcc0ab6e0296b4147f4106b22
yushengyun <yushengyun2013@163.com>
2020-08-31 00:40:40 +0800

    Date:20200815-1
    Info:
    Algorithm:0114
    CodingInterviews:0046
    CrakingTheCodingInterview:0103 0201

---
+++ src/algorithms/LeetCode0114.java
@@ -0,0 +1,55 @@
+package src.algorithms;
+
+import src.algorithms.baseclass.TreeNode;
+
+
+/**
+ *
+ * 114. 二叉树展开为链表
+ *
+ * 定一个二叉树，原地将它展开为一个单链表。
+ *
+ * 例如，给定二叉树
+ *
+ *     1
+ *    / \
+ *   2   5
+ *  / \   \
+ * 3   4   6
+ * 将其展开为：
+ *
+ * 1
+ *  \
+ *   2
+ *    \
+ *     3
+ *      \
+ *       4
+ *        \
+ *         5
+ *          \
+ *           6
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class LeetCode0114 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+
+}
