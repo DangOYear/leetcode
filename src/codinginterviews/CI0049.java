f3efa80e1892f302b6ea44448cf6f602e830951a
yushengyun <yushengyun2013@163.com>
2020-09-02 01:11:51 +0800

    Date:20200901-2
    Info:
    Algorithm:
    CodingInterviews:0049 0059I
    CrakingTheCodingInterview:

---
+++ src/codinginterviews/CI0049.java
@@ -0,0 +1,51 @@
+package src.codinginterviews;
+
+/**
+ *
+ * 剑指 Offer 49. 丑数
+ *
+ * 我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数。
+ *
+ *  
+ *
+ * 示例:
+ *
+ * 输入: n = 10
+ * 输出: 12
+ * 解释: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数。
+ * 说明:  
+ *
+ * 1 是丑数。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/chou-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class CI0049 {
+    class Solution {
+        public int nthUglyNumber(int n) {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int[] dp = new int[n];
+            dp[0] = 1;
+            for (int i = 1; i < n; ++i) {
+                int mul2 = dp[a] * 2;
+                int mul3 = dp[b] * 3;
+                int mul5 = dp[c] * 5;
+
+                dp[i] = Math.min(Math.min(mul2, mul3), mul5);
+                if (dp[i] == mul2)  ++a;
+                if (dp[i] == mul3)  ++b;
+                if (dp[i] == mul5)  ++c;
+            }
+            return dp[n-1];
+        }
+    }
+
+    public static void main(String[] args) {
+        CI0049.Solution solution = new CI0049().new Solution();
+        System.out.println(solution.nthUglyNumber(10));
+    }
+}
--- src/codinginterviews/CI0059I.java
+++ src/codinginterviews/CI0059I.java
@@ -1,6 +1,9 @@
 package src.codinginterviews;
 
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  *
  * 剑指 Offer 59 - I. 滑动窗口的最大值
@@ -36,7 +39,27 @@ public class CI0059I {
 
     class Solution {
         public int[] maxSlidingWindow(int[] nums, int k) {
-            return new int[]{1, 2, 3};
+            if (nums.length == 0 || k == 0) return new int[0];
+            Deque<Integer> deque = new LinkedList<>();
+            int[] res = new int[nums.length - k + 1];
+            for (int i = 0; i < k; ++i) {
+                while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                    deque.removeLast();
+                }
+                deque.addLast(nums[i]);
+            }
+            res[0] = deque.peekFirst();
+            for (int i = k; i < nums.length; ++i) {
+                if (deque.peekFirst() == nums[i-k]) {
+                    deque.removeFirst();
+                }
+                while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                    deque.removeLast();
+                }
+                deque.addLast(nums[i]);
+                res[i - k + 1] = deque.peekFirst();
+            }
+            return res;
         }
     }
 }
