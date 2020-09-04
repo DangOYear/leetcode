7051a5c139a212c8510c4e1f6a53b9337bbfc931
yushengyun <yushengyun2013@163.com>
2020-09-04 23:24:16 +0800

    Date:20200904-2
    Info:
    Algorithm:0172 0231 0908
    CodingInterviews:0056II
    CrakingTheCodingInterview:

--- src/algorithms/LeetCode0172.java
+++ src/algorithms/LeetCode0172.java
@@ -4,19 +4,14 @@ public class LeetCode0172 {
     class Solution {
         public int trailingZeroes(int n) {
             int res = 0;
-            for (int i = 1; i <= n; ++i) {
+            for (int i = 5; i <= n; i = i + 5) {
                 int N = i;
-                while (N > 0) {
-                    if (N % 5 == 0) {
-                        ++res;
-                        N /= 5;
-                    }else {
-                        break;
-                    }
+                while (N % 5 ==  0) {
+                    ++res;
+                    N /= 5;
                 }
             }
             return res;
         }
-
     }
 }
---
+++ src/algorithms/LeetCode0908.java
@@ -0,0 +1,15 @@
+package src.algorithms;
+
+public class LeetCode0908 {
+    class Solution {
+        public int smallestRangeI(int[] A, int K) {
+            int minNum = A[0];
+            int maxNum = A[0];
+            for (int num : A) {
+                minNum = Math.min(minNum, num);
+                maxNum = Math.max(maxNum, num);
+            }
+            return Math.max(0, maxNum - minNum - 2 * K);
+        }
+    }
+}
