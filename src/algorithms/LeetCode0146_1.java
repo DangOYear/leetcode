d9ef2967054c2e7ee2ef55a1feed5382dbd9dde4
yushengyun <yushengyun2013@163.com>
2020-09-26 19:45:14 +0800

    Date:20200926-1
    Info:
    Algorithm:0146 0221
    CodingInterviews:
    CrakingTheCodingInterview:
    MutilThread:

---
+++ src/algorithms/LeetCode0146_1.java
@@ -0,0 +1,29 @@
+package src.algorithms;
+
+import java.util.Map;
+
+public class LeetCode0146_1 {
+    class LRUCache {
+
+        Map<Integer>
+
+        public LRUCache(int capacity) {
+
+        }
+
+        public int get(int key) {
+
+        }
+
+        public void put(int key, int value) {
+
+        }
+    }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+}
