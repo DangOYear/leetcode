b35501333abbcbd4f6bd92dc65bb101c9d5e40c1
yushengyun <yushengyun2013@163.com>
2020-10-07 18:43:04 +0800

    Date:20201007-1
    Info:
    Algorithm:0075 1572
    CodingInterviews:
    CrakingTheCodingInterview:1709
    MutilThread:

---
+++ src/algorithms/LeetCode0075.java
@@ -0,0 +1,18 @@
+package src.algorithms;
+
+public class LeetCode0075 {
+    class Solution {
+        public void sortColors(int[] nums) {
+            int[] color = new int[3];
+            for (int x : nums) {
+                color[x]++;
+            }
+            int pos = 0;
+            for (int i = 0; i < color.length; ++i) {
+                for (int j = 0; j < color[i]; ++j) {
+                    nums[pos++] = i;
+                }
+            }
+        }
+    }
+}
