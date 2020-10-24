232e48a701e8de628f1b9a942f950e3b038005f3
yushengyun <yushengyun2013@163.com>
2020-10-24 10:05:53 +0800

    Date:20201024-1
    Info:
    Algorithm:1024
    CodingInterviews:
    CrakingTheCodingInterview:
    MutilThread:

---
+++ src/algorithms/LeetCode1024.java
@@ -0,0 +1,28 @@
+package src.algorithms;
+
+public class LeetCode1024 {
+    class Solution {
+        public int videoStitching(int[][] clips, int T) {
+            int[] maxRight = new int[T];
+            int last = 0;
+            int pre = 0;
+            int res = 0;
+            for (int[] clip : clips) {
+                if (clip[0] < T) {
+                    maxRight[clip[0]] = Math.max(maxRight[clip[0]], clip[1]);
+                }
+            }
+            for (int i = 0; i < T; ++i) {
+                last = Math.max(last, maxRight[i]);
+                if (i == last) {
+                    return -1;
+                }
+                if (i == pre) {
+                    ++res;
+                    pre = last;
+                }
+            }
+            return res;
+        }
+    }
+}
