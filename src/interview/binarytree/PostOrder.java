c65552ef55f8bf1f757d97ce01a801264b460d4e
yushengyun <yushengyun2013@163.com>
2020-11-06 22:19:38 +0800

    Date:20201106-2
    Info:
    Algorithm:
    CodingInterviews:
    CrakingTheCodingInterview:
    MutilThread:
    InterView:增加二叉树前序、中序、后序的递归形式

---
+++ src/interview/binarytree/PostOrder.java
@@ -0,0 +1,23 @@
+package src.interview.binarytree;
+
+import src.algorithms.baseclass.TreeNode;
+
+/**
+ * 后续遍历
+ *
+ */
+public class PostOrder {
+    /**
+     * 递归类型
+     */
+    public void postOrderRecursion(TreeNode node) {
+
+    }
+
+    /**
+     * 非递归遍历
+     */
+    public void postOrderNonRecursion(TreeNode node) {
+
+    }
+}
---
+++ src/interview/binarytree/PreOrder.java
@@ -0,0 +1,29 @@
+package src.interview.binarytree;
+
+import src.algorithms.baseclass.TreeNode;
+
+/**
+ * 前序遍历
+ *
+ */
+public class PreOrder {
+
+    /**
+     * 递归遍历
+     */
+    public void preOrderRecursion(TreeNode node) {
+        if (node != null) {
+            System.out.println(node.val);
+            preOrderRecursion(node.left);
+            preOrderRecursion(node.right);
+        }
+    }
+
+    /**
+     * 非递归遍历
+     */
+    public void preOrderNonRecursion(TreeNode node) {
+
+    }
+
+}
