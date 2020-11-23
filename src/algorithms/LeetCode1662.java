b29554fb69a084d67f647675b11ff18470bdb0e6
yushengyun <yushengyun2013@163.com>
2020-11-23 23:03:50 +0800

    Date:20201123-2
    Info:
    Algorithm:1614 1662
    CodingInterviews:
    CrakingTheCodingInterview:0109
    MutilThread:
    InterView:

---
+++ src/algorithms/LeetCode1662.java
@@ -0,0 +1,17 @@
+package src.algorithms;
+
+public class LeetCode1662 {
+    class Solution {
+        public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+            StringBuilder words1 = new StringBuilder();
+            StringBuilder words2 = new StringBuilder();
+            for (String str : word1) {
+                words1.append(str);
+            }
+            for (String str : word2) {
+                words2.append(str);
+            }
+            return words1.toString().equals(words2.toString());
+        }
+    }
+}
