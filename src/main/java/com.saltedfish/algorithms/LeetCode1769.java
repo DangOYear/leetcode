cdfde2e540c4f8fac2a5f640211b2aca94978526
ShengyunYu <yushengyun2013@163.com>
2021-02-28 14:21:07 +0800

    Date:2020228-1
    Info:
    Algorithm:5689
    CodingInterviews:
    CrakingTheCodingInterview:
    Database:
    MutilThread:
    InterView:

---
+++ src/main/java/com.saltedfish/algorithms/LeetCode1769.java
@@ -0,0 +1,24 @@
+package com.saltedfish.algorithms;
+
+/**
+ * @author SaltedFish
+ * @date 2021/2/24
+ */
+public class LeetCode1769 {
+    class Solution {
+        public int[] minOperations(String boxes) {
+            int[] res = new int[boxes.length()];
+            int left = 0, right = 0, total = 0;
+            if (boxes.charAt(0) == '1') {
+                ++left;
+            }
+            for (int i = 1; i < boxes.length(); ++i) {
+
+            }
+        }
+    }
+}
+
+
+
+
