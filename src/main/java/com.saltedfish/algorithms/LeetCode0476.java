681a47b713fa97d562ee62e6fdf668e8e1b310d7
saltedfish <yushengyun2013@163.com>
2021-03-17 22:42:22 +0800

    Date:2020317
    Info:
    Algorithm:0476 1009
    CodingInterviews:
    CrakingTheCodingInterview:
    Database:0627
    MutilThread:
    InterView:

--- src/main/java/com.saltedfish/algorithms/LeetCode0476.java
+++ src/main/java/com.saltedfish/algorithms/LeetCode0476.java
@@ -3,12 +3,20 @@ package com.saltedfish.algorithms;
 public class LeetCode0476 {
     class Solution {
         public int findComplement(int num) {
-            return ~num & Integer.toBinaryString(num).length();
+            int temp = num;
+            while ((temp & (temp - 1)) > 0) {
+                temp = temp & (temp - 1);
+            }
+            return ((temp << 1) - 1) ^ num;
         }
     }
 
 
     public static void main(String[] args) {
+        LeetCode0476.Solution solution = new LeetCode0476().new Solution();
+        System.out.println(solution.findComplement(1));
+        System.out.println(solution.findComplement(5));
+        System.out.println(solution.findComplement(2147483647));
 
     }
 }
