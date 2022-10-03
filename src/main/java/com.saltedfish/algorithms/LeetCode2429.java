e74a7160901d987837502dd770ee31419b4bcc74
ShengyunYu <yushengyun2013@163.com>
2022-10-03 19:50:02 +0800

    Date:20221003
    Info:
    Algorithm:2429
    CodingInterviews:
    CrakingTheCodingInterview:
    Database:
    MutilThread:
    InterView:

--- src/main/java/com.saltedfish/algorithms/LeetCode2429.java
+++ src/main/java/com.saltedfish/algorithms/LeetCode2429.java
@@ -3,9 +3,19 @@ package com.saltedfish.algorithms;
 public class LeetCode2429 {
     class Solution {
         public int minimizeXor(int num1, int num2) {
-            String biStr1 = Integer.bitCount(num1);
-            String biStr2 = Integer.bitCount(num2);
+            int biCnt1 = Integer.bitCount(num1);
+            int biCnt2 = Integer.bitCount(num2);
+            while (biCnt2 < biCnt1) {
+                num1 = num1 & (num1 - 1);
+                ++biCnt2;
+            }
 
+            while (biCnt1 < biCnt2) {
+                num1 = num1 | (num1 + 1);
+                ++biCnt1;
+            }
+
+            return num1;
         }
     }
 }
